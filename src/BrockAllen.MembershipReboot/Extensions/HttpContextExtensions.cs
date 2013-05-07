228ddf5dba6c6073dfae2d5c33408c49126361a7
Brock Allen <brockallen@gmail.com>
2013-05-07 16:25:55 -0400

    add helper to get application url from httpcontext

---
+++ src/BrockAllen.MembershipReboot/Extensions/HttpContextExtensions.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace BrockAllen.MembershipReboot
+{
+    public static class HttpContextExtensions
+    {
+        public static string GetApplicationUrl(this HttpContext ctx)
+        {
+            if (ctx == null) throw new ArgumentNullException("ctx");
+            
+            var request = ctx.Request;
+            
+            var baseUrl =
+                request.Url.Scheme +
+                "://" +
+                request.Url.Host + (request.Url.Port == 80 ? "" : ":" + request.Url.Port) +
+                request.ApplicationPath;
+            if (!baseUrl.EndsWith("/")) baseUrl += "/";
+            
+            return baseUrl;
+        }
+    }
+}
