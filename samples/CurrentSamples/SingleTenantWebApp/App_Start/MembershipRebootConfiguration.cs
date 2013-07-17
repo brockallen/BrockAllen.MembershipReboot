c3df65d37796378d6b842b80aa25d74fa489fbf6
Brock Allen <brockallen@gmail.com>
2013-07-17 03:46:02 -0400

    provide sample for new MembershipRebootConfiguration use

---
+++ samples/CurrentSamples/SingleTenantWebApp/App_Start/MembershipRebootConfiguration.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace BrockAllen.MembershipReboot.Mvc.App_Start
+{
+    public class PasswordValidator : IValidator
+    {
+        public ValidationResult Validate(UserAccountService service, UserAccount account, string value)
+        {
+            if (value.Length < 4)
+            {
+                return new ValidationResult("Password must be at least 4 characters long");
+            }
+            
+            return null;
+        }
+    }
+
+    public class MembershipRebootConfig
+    {
+        public static MembershipRebootConfiguration Create()
+        {
+            var settings = SecuritySettings.Instance;
+            settings.MultiTenant = false;
+            
+            var config = new MembershipRebootConfiguration(settings, new DelegateFactory(()=>new EFUserAccountRepository(settings.ConnectionStringName)));
+            config.RegisterPasswordValidator(new PasswordValidator());
+            var delivery = new SmtpMessageDelivery();
+            var formatter = new EmailMessageFormatter(new Lazy<ApplicationInformation>(() =>
+            {
+                // build URL
+                var baseUrl = HttpContext.Current.GetApplicationUrl();
+                // area name
+                baseUrl += "UserAccount/";
+
+                return new ApplicationInformation
+                {
+                    ApplicationName = "Test",
+                    LoginUrl = baseUrl + "Login",
+                    VerifyAccountUrl = baseUrl + "Register/Confirm/",
+                    CancelNewAccountUrl = baseUrl + "Register/Cancel/",
+                    ConfirmPasswordResetUrl = baseUrl + "PasswordReset/Confirm/",
+                    ConfirmChangeEmailUrl = baseUrl + "ChangeEmail/Confirm/"
+                };
+            }));
+            if (settings.RequireAccountVerification)
+            {
+                config.AddEventHandler(new EmailAccountCreatedEventHandler(formatter, delivery));
+            }
+            config.AddEventHandler(new EmailAccountEventsHandler(formatter, delivery));
+            
+            return config;
+        }
+
+        /*
+            kernel
+                .Bind<ApplicationInformation>()
+                .ToMethod(x=>
+                    {
+                       
+                    });
+        */
+    }
+}
---
+++ samples/CurrentSamples/SingleTenantWebApp/Areas/UserAccount/Models/LoginInputModel.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BrockAllen.MembershipReboot.Mvc.Areas.UserAccount.Models
+{
+    public class LoginInputModel
+    {
+        [Required]
+        public string Username { get; set; }
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+        [ScaffoldColumn(false)]
+        public string ReturnUrl { get; set; }
+    }
+}
