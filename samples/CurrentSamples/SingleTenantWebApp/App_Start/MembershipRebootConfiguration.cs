c68ef37aff9a7524013a08f5da6814756b5b7957
Brock Allen <brockallen@gmail.com>
2013-07-17 21:29:13 -0400

    simplifify basic configuration

--- samples/CurrentSamples/SingleTenantWebApp/App_Start/MembershipRebootConfiguration.cs
+++ samples/CurrentSamples/SingleTenantWebApp/App_Start/MembershipRebootConfiguration.cs
@@ -6,29 +6,13 @@ using System.Web;
 
 namespace BrockAllen.MembershipReboot.Mvc.App_Start
 {
-    public class PasswordValidator : IValidator
-    {
-        public ValidationResult Validate(UserAccountService service, UserAccount account, string value)
-        {
-            if (value.Length < 4)
-            {
-                return new ValidationResult("Password must be at least 4 characters long");
-            }
-            
-            return null;
-        }
-    }
-
     public class MembershipRebootConfig
     {
         public static MembershipRebootConfiguration Create()
         {
             var settings = SecuritySettings.Instance;
-            settings.MultiTenant = false;
-            
             var config = new MembershipRebootConfiguration(settings, new DelegateFactory(()=>new DefaultUserAccountRepository(settings.ConnectionStringName)));
-            config.RegisterPasswordValidator(new PasswordValidator());
-            var delivery = new SmtpMessageDelivery();
+            
             var formatter = new EmailMessageFormatter(new Lazy<ApplicationInformation>(() =>
             {
                 // build URL
@@ -48,9 +32,9 @@ namespace BrockAllen.MembershipReboot.Mvc.App_Start
             }));
             if (settings.RequireAccountVerification)
             {
-                config.AddEventHandler(new EmailAccountCreatedEventHandler(formatter, delivery));
+                config.AddEventHandler(new EmailAccountCreatedEventHandler(formatter));
             }
-            config.AddEventHandler(new EmailAccountEventsHandler(formatter, delivery));
+            config.AddEventHandler(new EmailAccountEventsHandler(formatter));
             
             return config;
         }
