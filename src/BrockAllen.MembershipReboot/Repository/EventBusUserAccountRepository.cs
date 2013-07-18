ffd07169588cbf5efc6da0b2861213945edb4049
Brock Allen <brockallen@gmail.com>
2013-07-17 21:04:04 -0400

    missed files on last checkin

---
+++ src/BrockAllen.MembershipReboot/Repository/EventBusUserAccountRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BrockAllen.MembershipReboot
+{
+    public class EventBusUserAccountRepository :
+       EventBusRepository<UserAccount>, IUserAccountRepository
+    {
+        public EventBusUserAccountRepository(IUserAccountRepository inner, IEventBus eventBus)
+            : base(inner, eventBus)
+        {
+        }
+    }
+}
