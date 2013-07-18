d891e470c22da7172c05785b74f28bd675e27f12
Brock Allen <brockallen@gmail.com>
2013-07-18 01:16:22 -0400

    reworked email message formatter to load from embedded resource files

--- src/BrockAllen.MembershipReboot/Notification/Email/EmailMessageFormatter.cs
+++ src/BrockAllen.MembershipReboot/Notification/Email/EmailMessageFormatter.cs
@@ -1,11 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace BrockAllen.MembershipReboot
-{ 
+{
     public class EmailMessageFormatter : IMessageFormatter
     {
         public class Tokenizer
@@ -30,7 +32,6 @@ namespace BrockAllen.MembershipReboot
                 return msg;
             }
         }
-
         public class EmailChangeRequestedTokenizer : Tokenizer
         {
             public override string Tokenize(UserAccountEvent accountEvent, ApplicationInformation appInfo, string msg)
@@ -53,7 +54,15 @@ namespace BrockAllen.MembershipReboot
                 return msg;
             }
         }
-        
+
+        public ApplicationInformation ApplicationInformation 
+        {
+            get
+            {
+                return appInfo.Value;
+            }
+        }
+
         Lazy<ApplicationInformation> appInfo;
         public EmailMessageFormatter(ApplicationInformation appInfo)
         {
@@ -69,192 +78,65 @@ namespace BrockAllen.MembershipReboot
         public Message Format(UserAccountEvent accountEvent)
         {
             if (accountEvent == null) throw new ArgumentNullException("accountEvent");
-
-            var type = accountEvent.GetType();
-            var msg = GetMessageFormat(type);
-            if (msg != null)
-            {
-                var tokenizer = GetTokenizer(type);
-                msg.Body = tokenizer.Tokenize(accountEvent, appInfo.Value, msg.Body);
-                msg.Subject = FormatSubject(accountEvent, appInfo.Value, msg.Subject);
-            }
-
-            return msg;
+            return CreateMessage(GetSubject(accountEvent), GetBody(accountEvent));
         }
 
-        protected virtual string FormatSubject(UserAccountEvent accountEvent, ApplicationInformation appInfo, string subject)
+        protected virtual Tokenizer GetTokenizer(UserAccountEvent evt)
         {
-            return String.Format("[{0}] {1}", appInfo.ApplicationName, subject);
-        }
-
-        protected Tokenizer GetTokenizer(Type type)
-        {
+            Type type = evt.GetType();
             if (type == typeof(EmailChangeRequestedEvent)) return new EmailChangeRequestedTokenizer();
             if (type == typeof(EmailChangedEvent)) return new EmailChangedTokenizer();
             return new Tokenizer();
         }
 
-        protected virtual Message GetMessageFormat(Type type)
+        protected Message CreateMessage(string subject, string body)
         {
-            if (type == typeof(AccountCreatedEvent)) return new Message { Body = GetAccountCreateFormat(), Subject = "Account Created" };
-            if (type == typeof(AccountVerifiedEvent)) return new Message { Body = GetAccountVerifiedFormat(), Subject = "Account Verified" };
-            if (type == typeof(PasswordResetRequestedEvent)) return new Message { Body = GetResetPasswordFormat(), Subject = "Password Reset Request" };
-            if (type == typeof(PasswordChangedEvent)) return new Message { Body = GetPasswordChangeNoticeFormat(), Subject = "Password Changed" };
-            if (type == typeof(UsernameReminderRequestedEvent)) return new Message { Body = GetAccountNameReminderFormat(), Subject = "Username Reminder" };
-            if (type == typeof(AccountClosedEvent)) return new Message { Body = GetAccountClosedFormat(), Subject = "Account Closed" };
-            if (type == typeof(EmailChangeRequestedEvent)) return new Message { Body = GetChangeEmailRequestNoticeFormat(), Subject = "Change Email Request" };
-            if (type == typeof(EmailChangedEvent)) return new Message { Body = GetEmailChangedNoticeFormat(), Subject = "Email Changed" };
-            if (type == typeof(UsernameChangedEvent)) return new Message { Body = GetUsernameChangedNoticeFormat(), Subject = "Username Changed" };
+            if (subject == null || body == null) return null;
 
-            if (type == typeof(AccountLockedEvent)) return null;
-            if (type == typeof(AccountNotVerifiedEvent)) return null;
-            if (type == typeof(FailedLoginEvent)) return null;
-            if (type == typeof(InvalidPasswordEvent)) return null;
-            if (type == typeof(SuccessfulLoginEvent)) return null;
-            if (type == typeof(TooManyRecentPasswordFailuresEvent)) return null;
+            return new Message { Subject = subject, Body = body };
+        }
+
+        protected string FormatValue(UserAccountEvent evt, string value)
+        {
+            if (value == null) return null;
+
+            var tokenizer = GetTokenizer(evt);
+            return tokenizer.Tokenize(evt, this.ApplicationInformation, value);
+        }
+        
+        protected virtual string GetSubject(UserAccountEvent evt)
+        {
+            return FormatValue(evt, LoadSubjectTemplate(evt));
+        }
+        protected virtual string GetBody(UserAccountEvent evt)
+        {
+            return FormatValue(evt, LoadBodyTemplate(evt));
+        }
+
+        protected virtual string LoadSubjectTemplate(UserAccountEvent evt)
+        {
+            return LoadTemplate(evt.GetType().Name + "_Subject");
+        }
+        protected virtual string LoadBodyTemplate(UserAccountEvent evt)
+        {
+            return LoadTemplate(evt.GetType().Name + "_Body");
+        }
+
+        const string ResourcePathTemplate = "BrockAllen.MembershipReboot.Notification.Email.EmailTemplates.{0}.txt";
+
+        protected virtual string LoadTemplate(string name)
+        {
+            name = String.Format(ResourcePathTemplate, name);
             
-            return null;
-        }
-
-        protected virtual string GetAccountCreateFormat()
-        {
-            return @"
-You (or someone) requested an account to be created with {applicationName}.
-
-Username: {username}
-
-Please click here to confirm your request so you can login: 
-
-{confirmAccountCreateUrl}
-
-If you did not create this account click here to cancel this request:
-
-{cancelNewAccountUrl}
-
-Thanks!
-
-{emailSignature}
-";
-        }
-
-        protected virtual string GetAccountVerifiedFormat()
-        {
-            return @"
-Your account has been verified with {applicationName}.
-
-Username: {username}
-
-You can now login at this address:
-
-{loginUrl}
-
-Thanks!
-
-{emailSignature}
-";
-        }
-
-        protected virtual string GetResetPasswordFormat()
-        {
-            return @"
-You (or someone else) has requested a password reset for {applicationName}.
-
-Username: {username}
-
-Please click here to confirm your request so you can reset your password: 
-
-{confirmPasswordResetUrl}
-
-Thanks!
-
-{emailSignature}
-";
-        }
-
-        protected virtual string GetPasswordChangeNoticeFormat()
-        {
-            return @"
-Your password has been changed at {applicationName}.
-
-Username: {username}
-
-Click here to login:
-
-{loginUrl}
-
-Thanks!
-
-{emailSignature}
-";
-        }
-        protected virtual string GetAccountNameReminderFormat()
-        {
-            return @"
-You (or someone else) requested a reminder for your username from {applicationName}.
-
-Username: {username}
-
-You can click here to login:
-
-{loginUrl}
-
-Thanks!
-
-{emailSignature}
-";
-        }
-        protected virtual string GetAccountClosedFormat()
-        {
-            return @"
-This email is to confirm that the account '{username}' has been closed for {applicationName}.
-
-Thanks!
-
-{emailSignature}
-";
-        }
-        protected virtual string GetChangeEmailRequestNoticeFormat()
-        {
-            return @"
-This email is to confirm an email change for your account with {applicationName}.
-
-Username: {username}
-Old Email: {oldEmail}
-New Email: {newEmail}
-
-Please click here to confirm your email change request:
-
-{confirmChangeEmailUrl}
-
-Thanks!
-
-{emailSignature}
-";
-        }
-        protected virtual string GetEmailChangedNoticeFormat()
-        {
-            return @"
-This email is to confirm that your email has been changed for your account with {applicationName}.
-
-Username: {username}
-New Email: {newEmail}
-
-Thanks!
-
-{emailSignature}
-";
-        }
-        protected virtual string GetUsernameChangedNoticeFormat()
-        {
-            return @"
-This email is to confirm that your username has been changed for your account with {applicationName}.
-
-New Username: {username}
-
-Thanks!
-
-{emailSignature}
-";
+            var asm = typeof(EmailMessageFormatter).Assembly;
+            using (var s = asm.GetManifestResourceStream(name))
+            {
+                if (s == null) return null;
+                using (var sr = new StreamReader(s))
+                {
+                    return sr.ReadToEnd();
+                }
+            }
         }
     }
 }
