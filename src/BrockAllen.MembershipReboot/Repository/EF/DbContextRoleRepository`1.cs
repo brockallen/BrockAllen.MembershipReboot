34c93cc7ad9ce8fcc0ed0681e8f1770ac91eb7f9
Brock Allen <brockallen@gmail.com>
2013-08-04 12:28:04 -0400

    Revert "Revert "add role model and role repository""
    This reverts commit 8a0e8995b4e5e78d97071f50413ba9e0e2729109.

---
+++ src/BrockAllen.MembershipReboot/Repository/EF/DbContextRoleRepository`1.cs
@@ -0,0 +1,22 @@
+﻿/*
+ * Copyright (c) Brock Allen.  All rights reserved.
+ * see license.txt
+ */
+
+using System.Data.Entity;
+namespace BrockAllen.MembershipReboot
+{
+    public class DbContextRoleRepository<Ctx>
+           : DbContextRepository<Role>, IRoleRepository
+        where Ctx : DbContext, new()
+    {
+        public DbContextRoleRepository()
+            : this(new Ctx())
+        {
+        }
+        public DbContextRoleRepository(Ctx ctx)
+            : base(ctx)
+        {
+        }
+    }
+}
