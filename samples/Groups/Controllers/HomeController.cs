a50d342a8b95ddcb91058c16efd9aa2b11372b9a
Brock Allen <brockallen@gmail.com>
2013-08-13 16:38:08 -0400

    updated sample UI to allow groups in groups

--- samples/Groups/Controllers/HomeController.cs
+++ samples/Groups/Controllers/HomeController.cs
@@ -1,11 +1,31 @@
 ﻿using BrockAllen.MembershipReboot;
 using System;
+using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web.Mvc;
 
 namespace RolesAdmin.Controllers
 {
+    public class GroupIndexViewModel
+    {
+        public IEnumerable<GroupViewModel> Groups { get; set; }
+        public IEnumerable<SelectListItem> GroupsAsList
+        {
+            get
+            {
+                return Groups.Select(x=>new SelectListItem { Text=x.Name, Value=x.ID.ToString() });
+            }
+        }
+    }
+
+    public class GroupViewModel
+    {
+        public Guid ID { get; set; }
+        public string Name { get; set; }
+        public IEnumerable<GroupViewModel> Children { get; set; }
+    }
+
     public class HomeController : Controller
     {
         GroupService groupSvc;
@@ -16,8 +36,26 @@ namespace RolesAdmin.Controllers
 
         public ActionResult Index()
         {
-            var groups = groupSvc.GetAll();
-            return View("Index", groups);
+            var list = new List<GroupViewModel>();
+            foreach(var item in groupSvc.GetAll())
+            {
+                var kids = new List<GroupViewModel>();
+                foreach (var child in item.Children)
+                {
+                    var childGrp = groupSvc.Get(child.ChildGroupID);
+                    kids.Add(new GroupViewModel { ID = child.ChildGroupID, Name = childGrp.Name });
+                }
+                var gvm = new GroupViewModel
+                {
+                    ID = item.ID, Name = item.Name,
+                    Children = kids
+                };
+                list.Add(gvm);
+            }
+            var vm = new GroupIndexViewModel { 
+                Groups = list
+            };
+            return View("Index", vm);
         }
 
         [HttpPost]
@@ -32,8 +70,7 @@ namespace RolesAdmin.Controllers
             {
                 ModelState.AddModelError("", ex.Message);
             }
-            var groups = groupSvc.GetAll();
-            return View("Index", groups);
+            return Index();
         }
 
         [HttpPost]
@@ -48,8 +85,7 @@ namespace RolesAdmin.Controllers
             {
                 ModelState.AddModelError("", ex.Message);
             }
-            var groups = groupSvc.GetAll();
-            return View("Index", groups);
+            return Index();
         }
 
         [HttpPost]
@@ -64,8 +100,36 @@ namespace RolesAdmin.Controllers
             {
                 ModelState.AddModelError("", ex.Message);
             }
-            var groups = groupSvc.GetAll();
-            return View("Index", groups);
+            return Index();
+        }
+
+        [HttpPost]
+        public ActionResult AddChild(Guid id, Guid child)
+        {
+            try
+            {
+                groupSvc.AddChildGroup(id, child);
+                return RedirectToAction("Index");
+            }
+            catch (ValidationException ex)
+            {
+                ModelState.AddModelError("", ex.Message);
+            }
+            return Index();
+        }
+        [HttpPost]
+        public ActionResult RemoveChild(Guid id, Guid child)
+        {
+            try
+            {
+                groupSvc.RemoveChildGroup(id, child);
+                return RedirectToAction("Index");
+            }
+            catch (ValidationException ex)
+            {
+                ModelState.AddModelError("", ex.Message);
+            }
+            return Index();
         }
     }
 }
