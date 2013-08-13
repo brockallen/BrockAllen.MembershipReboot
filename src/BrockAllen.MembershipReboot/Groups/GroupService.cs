d1257b4431a27fa4fd780070d4caf3588a5a226d
Brock Allen <brockallen@gmail.com>
2013-08-13 01:23:56 -0400

    vs project save fail

---
+++ src/BrockAllen.MembershipReboot/Groups/GroupService.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BrockAllen.MembershipReboot
+{
+    public class GroupService
+    {
+        IGroupRepository groupRepository;
+        public GroupService(IGroupRepository groupRepository)
+        {
+            if (groupRepository == null) throw new ArgumentNullException("groupRepository");
+
+            this.groupRepository = groupRepository;
+        }
+
+
+    }
+}
