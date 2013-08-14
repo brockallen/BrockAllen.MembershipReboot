f2db15c53fd6af62136683236e7af6c3a6252bf1
Chris Keenan <chris.keenan@yahoo.com>
2013-08-14 14:17:37 -0400

    Initial commit of RavenDb provider.

---
+++ src/BrockAllen.MembershipReboot.RavenDb/RavenDbGroupRepository`1.cs
@@ -0,0 +1,19 @@
+﻿/*
+ * Copyright (c) Brock Allen.  All rights reserved.
+ * see license.txt
+ */
+
+using Raven.Client;
+
+namespace BrockAllen.MembershipReboot.RavenDb
+{
+    public class RavenDbGroupRepository<Ctx>
+        : RavenDbRepository<Group>, IGroupRepository
+        where Ctx : IDocumentStore, new()
+    {
+        public RavenDbGroupRepository(Ctx ctx)
+            : base(ctx)
+        {
+        }
+    }
+}
