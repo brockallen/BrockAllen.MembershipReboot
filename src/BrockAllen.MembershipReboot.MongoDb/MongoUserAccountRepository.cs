0fb3f58848bc684bc2bd7cce2aceb188b9816b96
Jason Diamond <jason@diamond.name>
2013-08-14 19:33:20 -0700

    MongoDB repository

---
+++ src/BrockAllen.MembershipReboot.MongoDb/MongoUserAccountRepository.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Linq;
+using MongoDB.Driver.Builders;
+
+namespace BrockAllen.MembershipReboot.MongoDb
+{
+    public class MongoUserAccountRepository : IUserAccountRepository
+    {
+        private readonly MongoDatabase _db;
+
+        public MongoUserAccountRepository(MongoDatabase db)
+        {
+            _db = db;
+        }
+
+        public UserAccount Create()
+        {
+            return new UserAccount();
+        }
+        
+        public IQueryable<UserAccount> GetAll()
+        {
+            return _db.Users().FindAll().AsQueryable();
+        }
+
+        public UserAccount Get(params object[] keys)
+        {
+            return _db.Users().FindOne(Query<UserAccount>.EQ(e => e.ID, (Guid)keys[0]));
+        }
+
+        public void Add(UserAccount item)
+        {
+            _db.Users().Insert(item);
+        }
+
+        public void Update(UserAccount item)
+        {
+            _db.Users().Save(item);
+        }
+
+        public void Remove(UserAccount item)
+        {
+            _db.Users().Remove(Query<UserAccount>.EQ(e => e.ID, item.ID));
+        }
+
+        public void Dispose()
+        {
+        }
+    }
+}
