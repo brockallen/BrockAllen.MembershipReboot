45b1c2dcf25ae19625bbfd9ee77f64fceaa342da
Brock Allen <brockallen@gmail.com>
2013-11-27 12:10:55 -0500

    better comment

--- src/BrockAllen.MembershipReboot/AccountService/UserAccountEvents.cs
+++ src/BrockAllen.MembershipReboot/AccountService/UserAccountEvents.cs
@@ -16,7 +16,9 @@ namespace BrockAllen.MembershipReboot
 
     public class AccountCreatedEvent<T> : UserAccountEvent<T>
     {
-        // InitialPassword might be null
+        // InitialPassword might be null if this is a re-send
+        // notification for account created (when user tries to
+        // reset password before verifying their account)
         public string InitialPassword { get; set; }
         public string VerificationKey { get; set; }
     }
