8452e0e2492cf27e2aab0bbca286ca84953b85fa
d.andresini <damianoandresini@hotmail.com>
2013-12-19 15:50:33 +0100

    Smtp remove error
    If smtpclient go on error every call stopped to execute

--- src/BrockAllen.MembershipReboot/Notification/Email/SmtpMessageDelivery.cs
+++ src/BrockAllen.MembershipReboot/Notification/Email/SmtpMessageDelivery.cs
@@ -10,6 +10,7 @@ using System.Net.Mail;
 
 namespace BrockAllen.MembershipReboot
 {
+  
     public class SmtpMessageDelivery : IMessageDelivery
     {
         public void Send(Message msg)
@@ -18,11 +19,24 @@ namespace BrockAllen.MembershipReboot
             {
                 SmtpSection smtp = ConfigurationManager.GetSection("system.net/mailSettings/smtp") as SmtpSection;
                 msg.From = smtp.From;
+                
             }
 
             using (SmtpClient smtp = new SmtpClient())
             {
-                smtp.Send(msg.From, msg.To, msg.Subject, msg.Body);
+                smtp.Timeout = 5000;
+                try
+                {
+                    
+                    smtp.Send(msg.From, msg.To, msg.Subject, msg.Body);
+                }
+                catch (SmtpException e)
+                {
+                    Tracing.Verbose("Error in Send Mail: " + e.Message);
+                }
+                catch (Exception e) {
+                    Tracing.Verbose("Error in Send Mail: " + e.Message);
+                }
             }
         }
     }
