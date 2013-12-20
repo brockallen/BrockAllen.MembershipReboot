6cecf9c5d380dba6ca1cede33e189ad52efb7691
Brock Allen <brockallen@gmail.com>
2013-12-19 22:17:11 -0500

    add admin area back into sample

---
+++ samples/CustomizationsSample/CustomizationsSample/Areas/Admin/Controllers/HomeController.cs
@@ -0,0 +1,31 @@
+﻿using BrockAllen.MembershipReboot.Ef;
+using System;
+using System.Linq;
+using System.Web.Mvc;
+
+namespace BrockAllen.MembershipReboot.Mvc.Areas.Admin.Controllers
+{
+    public class HomeController : Controller
+    {
+        CustomDatabase db;
+        public HomeController()
+        {
+            this.db = new CustomDatabase();
+        }
+
+        public ActionResult Index()
+        {
+            var names =
+                from a in db.UserAccountsTableWithSomeOtherName
+                select a;
+            return View(names.ToArray());
+        }
+
+        public ActionResult Detail(Guid id)
+        {
+            var account = db.UserAccountsTableWithSomeOtherName.Find(id);
+            return View("Detail", account);
+        }
+
+    }
+}
