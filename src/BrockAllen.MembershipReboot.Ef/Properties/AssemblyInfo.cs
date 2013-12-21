f0a14ccee78fd065e0ab40339c797f17944babbd
Brock Allen <brockallen@gmail.com>
2013-12-21 14:49:08 -0500

    v5.0.1 for azure migrations

--- src/BrockAllen.MembershipReboot.Ef/Properties/AssemblyInfo.cs
+++ src/BrockAllen.MembershipReboot.Ef/Properties/AssemblyInfo.cs
@@ -30,5 +30,5 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("5.0.0.0")]
-[assembly: AssemblyFileVersion("5.0.0.0")]
+[assembly: AssemblyVersion("5.0.1.0")]
+[assembly: AssemblyFileVersion("5.0.1.0")]
