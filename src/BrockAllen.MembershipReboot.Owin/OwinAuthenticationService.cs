93d881fab7876c5eb7d566cbda9609712eccdccf
Brock Allen <brockallen@gmail.com>
2013-12-22 09:24:27 -0500

    add back in generic OwinAuthenticationService type

--- src/BrockAllen.MembershipReboot.Owin/OwinAuthenticationService.cs
+++ src/BrockAllen.MembershipReboot.Owin/OwinAuthenticationService.cs
@@ -67,9 +67,87 @@ namespace BrockAllen.MembershipReboot.Owin
             var props = new AuthenticationProperties();
             if (tokenLifetime.HasValue) props.ExpiresUtc = DateTime.UtcNow.Add(tokenLifetime.Value);
             if (persistentCookie.HasValue) props.IsPersistent = persistentCookie.Value;
-            
+
             var id = new ClaimsIdentity(enumerable, authType);
-            
+
+            context.Authentication.SignIn(props, id);
+        }
+
+        protected override void RevokeToken()
+        {
+            context.Authentication.SignOut(
+                this.authenticationType,
+                MembershipRebootOwinConstants.AuthenticationTwoFactorType);
+        }
+
+        protected override ClaimsPrincipal GetCurentPrincipal()
+        {
+            var cp = context.Request.User as ClaimsPrincipal;
+            if (cp == null) cp = new ClaimsPrincipal(context.Request.User);
+            return cp;
+        }
+    }
+    
+    public class OwinAuthenticationService<TAccount> : AuthenticationService<TAccount>
+        where TAccount : UserAccount
+    {
+        IOwinContext context;
+        string authenticationType;
+
+        public OwinAuthenticationService(
+            string authenticationType,
+            UserAccountService<TAccount> svc,
+            IDictionary<string, object> env,
+            ClaimsAuthenticationManager transformer
+        )
+            : base(svc, transformer)
+        {
+            this.authenticationType = authenticationType;
+            context = new OwinContext(env);
+        }
+
+        public OwinAuthenticationService(
+            string authenticationType,
+            UserAccountService<TAccount> svc,
+            IDictionary<string, object> env
+        )
+            : this(authenticationType, svc, env, null)
+        {
+        }
+
+        public OwinAuthenticationService(
+            UserAccountService<TAccount> svc,
+            IDictionary<string, object> env
+        )
+            : this(MembershipRebootOwinConstants.AuthenticationType, svc, env, null)
+        {
+        }
+
+        protected override void IssueToken(System.Security.Claims.ClaimsPrincipal principal,
+            TimeSpan? tokenLifetime = null, bool? persistentCookie = null)
+        {
+            if (principal == null) throw new ArgumentNullException("principal");
+
+            if (principal.Identity.IsAuthenticated)
+            {
+                IssueCookie(principal.Claims, authenticationType, tokenLifetime, persistentCookie);
+            }
+            else
+            {
+                IssueCookie(principal.Claims, MembershipRebootOwinConstants.AuthenticationTwoFactorType, tokenLifetime, persistentCookie);
+            }
+        }
+
+        private void IssueCookie(IEnumerable<Claim> enumerable, string authType, TimeSpan? tokenLifetime, bool? persistentCookie)
+        {
+            SignOut();
+
+            var props = new AuthenticationProperties();
+            if (tokenLifetime.HasValue) props.ExpiresUtc = DateTime.UtcNow.Add(tokenLifetime.Value);
+            if (persistentCookie.HasValue) props.IsPersistent = persistentCookie.Value;
+
+            var id = new ClaimsIdentity(enumerable, authType);
+
             context.Authentication.SignIn(props, id);
         }
 
