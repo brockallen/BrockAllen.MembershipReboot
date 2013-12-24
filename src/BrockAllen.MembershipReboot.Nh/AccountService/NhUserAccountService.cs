19a1d64131ed5fc878e136a5afa135a6263828a7
Caleb Kiage <caleb.kiage@gmail.com>
2013-12-24 23:08:24 +0300

    Add UserAccountService implementation.

---
+++ src/BrockAllen.MembershipReboot.Nh/AccountService/NhUserAccountService.cs
@@ -0,0 +1,30 @@
+﻿namespace BrockAllen.MembershipReboot.Nh
+{
+    using BrockAllen.MembershipReboot;
+
+    public class NhUserAccountService<TAccount> : UserAccountService<TAccount> where TAccount : UserAccount
+    {
+        public NhUserAccountService(IUserAccountRepository<TAccount> userRepository)
+            : base(userRepository)
+        {
+        }
+
+        public NhUserAccountService(MembershipRebootConfiguration<TAccount> configuration, IUserAccountRepository<TAccount> userRepository)
+            : base(configuration, userRepository)
+        {
+        }
+    }
+
+    public class NhUserAccountService : NhUserAccountService<NhUserAccount>
+    {
+        public NhUserAccountService(IUserAccountRepository<NhUserAccount> userRepository)
+            : this(new MembershipRebootConfiguration<NhUserAccount>(), userRepository)
+        {
+        }
+
+        public NhUserAccountService(MembershipRebootConfiguration<NhUserAccount> configuration, IUserAccountRepository<NhUserAccount> userRepository)
+            : base(configuration, userRepository)
+        {
+        }
+    }
+}
