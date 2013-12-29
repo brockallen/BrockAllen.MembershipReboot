925ef98b85545b026d1be418bf61725069735d7c
Caleb Kiage <caleb.kiage@gmail.com>
2013-12-29 08:27:48 +0300

    Remove NHibernate Profiler.

--- samples/NhibernateSample/NhibernateSample/Global.asax.cs
+++ samples/NhibernateSample/NhibernateSample/Global.asax.cs
@@ -12,8 +12,6 @@ namespace NhibernateSample
     {
         protected void Application_Start()
         {
-            HibernatingRhinos.Profiler.Appender.NHibernate.NHibernateProfiler.Initialize();
-
             AreaRegistration.RegisterAllAreas();
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
