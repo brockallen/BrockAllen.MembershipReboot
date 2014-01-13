dcc54e15d58113f4217e43b7423a5ab6edf20873
Brock Allen <brockallen@gmail.com>
2014-01-13 13:37:29 -0500

    config extensions for handlers

--- src/BrockAllen.MembershipReboot/Extensions/ConfigurationExtensions.cs
+++ src/BrockAllen.MembershipReboot/Extensions/ConfigurationExtensions.cs
@@ -22,5 +22,29 @@ namespace BrockAllen.MembershipReboot
             if (config == null) throw new ArgumentNullException("config");
             config.RegisterPasswordValidator(new PasswordComplexityValidator<TAccount>(minimumLength, minimumNumberOfComplexityRules));
         }
+
+        public static void AddCommandHandler<TAccount, TCommand>(this MembershipRebootConfiguration<TAccount> config, Action<TCommand> action)
+            where TAccount : UserAccount
+            where TCommand : ICommand
+        {
+            if (config == null) throw new ArgumentNullException("config");
+            config.AddCommandHandler(new DelegateCommandHandler<TCommand>(action));
+        }
+
+        public static void AddValidationHandler<TAccount, TEvent>(this MembershipRebootConfiguration<TAccount> config, Action<TEvent> action)
+            where TAccount : UserAccount
+            where TEvent : IEvent
+        {
+            if (config == null) throw new ArgumentNullException("config");
+            config.AddValidationHandler(new DelegateEventHandler<TEvent>(action));
+        }
+
+        public static void AddEventHandler<TAccount, TEvent>(this MembershipRebootConfiguration<TAccount> config, Action<TEvent> action)
+            where TAccount : UserAccount
+            where TEvent : IEvent
+        {
+            if (config == null) throw new ArgumentNullException("config");
+            config.AddEventHandler(new DelegateEventHandler<TEvent>(action));
+        }
     }
 }
