2f49c6ee217e43f60a4a60c6ea90851869e02734
Brock Allen <brockallen@gmail.com>
2014-01-14 09:52:36 -0500

    update v6 migration for username/email column change

--- src/BrockAllen.MembershipReboot.Ef/Migrations/201312281555533_v6.cs
+++ src/BrockAllen.MembershipReboot.Ef/Migrations/201312281555533_v6.cs
@@ -7,10 +7,14 @@ namespace BrockAllen.MembershipReboot.Ef.Migrations
     {
         public override void Up()
         {
+            AlterColumn("dbo.UserAccounts", "Username", c => c.String(nullable: false, maxLength: 254));
+            AlterColumn("dbo.UserAccounts", "Email", c => c.String(maxLength: 254));
         }
         
         public override void Down()
         {
+            AlterColumn("dbo.UserAccounts", "Email", c => c.String(maxLength: 100));
+            AlterColumn("dbo.UserAccounts", "Username", c => c.String(nullable: false, maxLength: 100));
         }
     }
 }
