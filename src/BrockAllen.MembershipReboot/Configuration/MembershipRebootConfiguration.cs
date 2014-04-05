f3efd72fb8e875a0eb88773c6021b04e8f971557
Brock Allen <brockallen@gmail.com>
2014-04-05 11:48:22 -0400

    add verification checks that event handlers are using the right user account type

--- src/BrockAllen.MembershipReboot/Configuration/MembershipRebootConfiguration.cs
+++ src/BrockAllen.MembershipReboot/Configuration/MembershipRebootConfiguration.cs
@@ -71,6 +71,7 @@ namespace BrockAllen.MembershipReboot
         public IEventBus EventBus { get { return eventBus; } }
         public void AddEventHandler(params IEventHandler[] handlers)
         {
+            foreach (var h in handlers) VerifyHandler(h);
             eventBus.AddRange(handlers);
         }
         
@@ -78,6 +79,7 @@ namespace BrockAllen.MembershipReboot
         public IEventBus ValidationBus { get { return validationBus; } }
         public void AddValidationHandler(params IEventHandler[] handlers)
         {
+            foreach (var h in handlers) VerifyHandler(h);
             validationBus.AddRange(handlers);
         }
 
@@ -85,7 +87,55 @@ namespace BrockAllen.MembershipReboot
         public ICommandBus CommandBus { get { return commandBus; } }
         public void AddCommandHandler(ICommandHandler handler)
         {
+            VerifyHandler(handler);
             commandBus.Add(handler);
+        }
+
+        private void VerifyHandler(IEventHandler e)
+        {
+            var type = e.GetType();
+            var interfaces = type.GetInterfaces();
+            foreach (var itf in interfaces)
+            {
+                if (itf.IsGenericType && itf.GetGenericTypeDefinition() == typeof(IEventHandler<>))
+                {
+                    var eventHandlerType = itf.GetGenericArguments()[0];
+                    if (eventHandlerType.IsGenericType)
+                    {
+                        var targetUserAccountType = eventHandlerType.GetGenericArguments()[0];
+                        var isSameType = targetUserAccountType == typeof(TAccount);
+                        if (!isSameType)
+                        {
+                            throw new ArgumentException(String.Format("Event handler: {0} must handle events for User Account type: {1}",
+                                e.GetType().FullName,
+                                typeof(TAccount).FullName));
+                        }
+                    }
+                }
+            }
+        }
+        private void VerifyHandler(ICommandHandler e)
+        {
+            var type = e.GetType();
+            var interfaces = type.GetInterfaces();
+            foreach (var itf in interfaces)
+            {
+                if (itf.IsGenericType && itf.GetGenericTypeDefinition() == typeof(ICommandHandler<>))
+                {
+                    var eventHandlerType = itf.GetGenericArguments()[0];
+                    if (eventHandlerType.IsGenericType)
+                    {
+                        var targetUserAccountType = eventHandlerType.GetGenericArguments()[0];
+                        var isSameType = targetUserAccountType == typeof(TAccount);
+                        if (!isSameType)
+                        {
+                            throw new ArgumentException(String.Format("Command handler: {0} must handle commands for User Account type: {1}",
+                                e.GetType().FullName,
+                                typeof(TAccount).FullName));
+                        }
+                    }
+                }
+            }
         }
         
         public ICrypto Crypto { get; set; }
