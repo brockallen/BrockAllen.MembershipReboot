5d1099f8b86e3171c2ca8ae2f609c072b67b8745
Brock Allen <brockallen@gmail.com>
2014-09-26 12:52:45 -0400

    allow groups to be updated

---
+++ samples/Groups/Groups/Models/CustomGroup.cs
@@ -0,0 +1,47 @@
+﻿using BrockAllen.MembershipReboot;
+using BrockAllen.MembershipReboot.Ef;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace RolesAdmin.Models
+{
+    public class CustomGroup : RelationalGroup
+    {
+        public string Description { get; set; }
+    }
+
+    public class CustomGroupDbContext : DbContext
+    {
+        public CustomGroupDbContext ()
+            : base("CustomGroup")
+	    {
+	    }
+
+        public DbSet<CustomGroup> Groups { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.ConfigureMembershipRebootGroups<CustomGroup>();
+        }
+    }
+
+    public class CustomGroupRepository : DbContextGroupRepository<CustomGroupDbContext, CustomGroup>
+    {
+    }
+
+    public static class CustomGroupTest
+    {
+        public static void Test()
+        {
+            var repo = new CustomGroupRepository();
+            var grpSvc = new GroupService<CustomGroup>("tenant", repo);
+            var g = grpSvc.Create("Foo");
+            g.Description = "Bar";
+            grpSvc.Update(g);
+        }
+    }
+}
