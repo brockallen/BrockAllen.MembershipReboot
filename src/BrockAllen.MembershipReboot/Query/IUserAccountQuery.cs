8e540596a071e356c8c718d2db46d937155a23cb
Brock Allen <brockallen@gmail.com>
2014-09-26 20:41:02 -0400

    split into separate files

--- src/BrockAllen.MembershipReboot/Query/IUserAccountQuery.cs
+++ src/BrockAllen.MembershipReboot/Query/IUserAccountQuery.cs
@@ -16,11 +16,4 @@ namespace BrockAllen.MembershipReboot
         IEnumerable<UserAccountQueryResult> Query(string filter, int skip, int count, out int totalCount);
         IEnumerable<UserAccountQueryResult> Query(string tenant, string filter, int skip, int count, out int totalCount);
     }
-    
-    public interface IUserAccountQuery<TAccount>
-        where TAccount : UserAccount
-    {
-        IEnumerable<UserAccountQueryResult> Query(Func<IQueryable<TAccount>, IQueryable<TAccount>> filter);
-        IEnumerable<UserAccountQueryResult> Query(Func<IQueryable<TAccount>, IQueryable<TAccount>> filter, Func<IQueryable<TAccount>, IOrderedQueryable<TAccount>> sort, int skip, int count, out int totalCount);
-    }
 }
---
+++ src/BrockAllen.MembershipReboot/Query/IUserAccountQuery`1.cs
@@ -0,0 +1,17 @@
+﻿/*
+ * Copyright (c) Brock Allen.  All rights reserved.
+ * see license.txt
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+namespace BrockAllen.MembershipReboot
+{
+    public interface IUserAccountQuery<TAccount>
+        where TAccount : UserAccount
+    {
+        IEnumerable<UserAccountQueryResult> Query(Func<IQueryable<TAccount>, IQueryable<TAccount>> filter);
+        IEnumerable<UserAccountQueryResult> Query(Func<IQueryable<TAccount>, IQueryable<TAccount>> filter, Func<IQueryable<TAccount>, IOrderedQueryable<TAccount>> sort, int skip, int count, out int totalCount);
+    }
+}
