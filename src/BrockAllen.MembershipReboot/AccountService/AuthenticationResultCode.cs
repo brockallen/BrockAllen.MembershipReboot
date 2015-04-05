684acf06f6a10e323fa33018fbb75a401827e7fb
ccrowhurstram <ccrowhurst@ramplc.com>
2015-04-05 20:08:37 +0100

    feat(UserAccountService): Add overloads to Authenticate method that return a failure code

---
+++ src/BrockAllen.MembershipReboot/AccountService/AuthenticationResultCode.cs
@@ -0,0 +1,14 @@
+﻿namespace BrockAllen.MembershipReboot
+{
+    public enum AuthenticationFailureCode
+    {
+        None,
+        AccountClosed,
+        AccountNotConfiguredWithCertificates,
+        AccountNotConfiguredWithMobilePhone,
+        AccountNotVerified,
+        FailedLoginAttemptsExceeded,
+        InvalidCredentials,
+        LoginNotAllowed
+    }
+}
