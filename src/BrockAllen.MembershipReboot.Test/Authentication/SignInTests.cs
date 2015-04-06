f7ccd0108281cb7535a5461461f7912cb7eca387
ccrowhurstram <ccrowhurst@ramplc.com>
2015-04-06 13:00:02 +0100

    feat(AuthenticationService): SignIn adds partial authentication claims when user not fully authenticated

---
+++ src/BrockAllen.MembershipReboot.Test/Authentication/SignInTests.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Globalization;
+using System.Linq;
+using System.Security.Claims;
+using BrockAllen.MembershipReboot.Relational;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace BrockAllen.MembershipReboot.Test.Authentication
+{
+    [TestClass]
+    public class SignInTests
+    {
+        TestAuthenticationService subject;
+        UserAccountService userAccountService;
+        FakeUserAccountRepository repository;
+
+        [TestInitialize]
+        public void Init()
+        {
+            SecuritySettings.Instance.PasswordHashingIterationCount = 1; // tests will run faster
+
+            var configuration = new MembershipRebootConfiguration
+            {
+                RequireAccountVerification = false,
+                PasswordResetFrequency = 1 // every day
+            };
+            configuration.AddEventHandler(new KeyNotification());
+            configuration.AddCommandHandler(new TestMapClaimsFromAccount());
+            repository = new FakeUserAccountRepository();
+            userAccountService = new UserAccountService(configuration, repository);
+
+            subject = new TestAuthenticationService(userAccountService);
+        }
+
+        [TestMethod]
+        public void SignedIn_ClaimsPrinicipal_Claims()
+        {
+            // given
+            UserAccount acc = userAccountService.CreateAccount("test", "abcdefg123", "test@gmail.com");
+            acc.MobilePhoneNumber = "07506777777";
+
+            // when
+            subject.SignIn(acc, "Bearer");
+
+            // then
+            AssertHasBasicClaims(subject.CurentPrincipal);
+            Assert.IsTrue(subject.CurentPrincipal.HasClaim(ClaimTypes.Email));
+            Assert.IsTrue(subject.CurentPrincipal.HasClaim(ClaimTypes.MobilePhone));
+            Assert.IsTrue(subject.CurentPrincipal.HasClaim("TestClaim"));
+        }
+
+
+        [TestMethod]
+        public void SignedIn_RequiresTwoFactorAuthToSignIn_Claims()
+        {
+            // given... 
+
+            UserAccount acc = userAccountService.CreateAccount("test", "abcdefg123", "test@gmail.com");
+            acc.MobilePhoneNumber = "07506777777";
+            acc.AccountTwoFactorAuthMode = TwoFactorAuthMode.Mobile;
+            // fake a request for two-factor auth
+            acc.CurrentTwoFactorAuthStatus = TwoFactorAuthMode.Mobile;
+
+            // when
+            subject.SignIn(acc, "Bearer");
+
+            // then
+            AssertHasBasicClaims(subject.CurentPrincipal);
+            Assert.AreEqual(PartialAuthReason.PendingTwoFactorAuth, subject.CurentPrincipal.GetPartialAuthReason());
+            Assert.AreEqual(TwoFactorAuthMode.Mobile.ToString(), 
+                subject.CurentPrincipal.Claims.GetValue(MembershipRebootConstants.ClaimTypes.PendingTwoFactorAuth));
+            Assert.IsFalse(subject.CurentPrincipal.HasClaim("TestClaim"));
+
+        }
+
+        [TestMethod]
+        public void SignedIn_RequiresPasswordReset_Claims()
+        {
+            // given... 
+            UserAccount acc = userAccountService.CreateAccount("test", "abcdefg123", "test@gmail.com");
+            acc.RequiresPasswordReset = true;
+
+            // when
+            subject.SignIn(acc, "Bearer");
+
+            // then
+            AssertHasBasicClaims(subject.CurentPrincipal);
+            Assert.AreEqual(PartialAuthReason.PasswordResetRequired, subject.CurentPrincipal.GetPartialAuthReason());
+            Assert.IsFalse(subject.CurentPrincipal.HasClaim("TestClaim"));
+        }
+
+        [TestMethod]
+        public void SignedIn_PasswordExpired_Claims()
+        {
+            // given... 
+            UserAccount acc = userAccountService.CreateAccount("test", "abcdefg123", "test@gmail.com");
+            acc.PasswordChanged = DateTime.Now.AddMonths(-1);
+
+            // when
+            subject.SignIn(acc, "Bearer");
+
+            // then
+            AssertHasBasicClaims(subject.CurentPrincipal);
+            Assert.AreEqual(PartialAuthReason.PasswordExpired, subject.CurentPrincipal.GetPartialAuthReason());
+            Assert.IsFalse(subject.CurentPrincipal.HasClaim("TestClaim"));
+        }
+
+        private void AssertHasBasicClaims(ClaimsPrincipal signingInUser)
+        {
+            Assert.AreEqual("Bearer", signingInUser.Claims.GetValue(ClaimTypes.AuthenticationMethod));
+            Assert.IsTrue(signingInUser.HasClaim(ClaimTypes.AuthenticationInstant));
+            Assert.IsTrue(signingInUser.HasClaim(ClaimTypes.NameIdentifier));
+            Assert.IsTrue(signingInUser.HasClaim(ClaimTypes.Name));
+            Assert.IsTrue(signingInUser.HasClaim(MembershipRebootConstants.ClaimTypes.Tenant));
+        }
+
+        public class TestMapClaimsFromAccount : ICommandHandler<MapClaimsFromAccount<UserAccount>>
+        {
+            public void Handle(MapClaimsFromAccount<UserAccount> cmd)
+            {
+                cmd.MappedClaims = new[] { new Claim("TestClaim", "TestClaim") };
+            }
+        }
+    }
+}
