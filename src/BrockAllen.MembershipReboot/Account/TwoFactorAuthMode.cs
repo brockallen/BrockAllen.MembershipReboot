6f253dc9a4b570764b1dd110342e96b8f60612b1
Link, Eric <eric.link@tylertech.com>
2016-05-20 15:31:11 -0500

    Add time based, Voice and Email options to the TwoFactorAuthMode enum.  This will allow us to use the extended values in our user service and controller in identity server, and in our event listeners that send codes via these methods.

--- src/BrockAllen.MembershipReboot/Account/TwoFactorAuthMode.cs
+++ src/BrockAllen.MembershipReboot/Account/TwoFactorAuthMode.cs
@@ -2,13 +2,15 @@
  * Copyright (c) Brock Allen.  All rights reserved.
  * see license.txt
  */
-
 namespace BrockAllen.MembershipReboot
 {
-    public enum TwoFactorAuthMode
-    {
-        None = 0,
-        Mobile = 1,
-        Certificate = 2
-    }
+  public enum TwoFactorAuthMode
+  {
+    None = 0,
+    Mobile = 1,
+    Certificate = 2,
+    TimeBasedToken = 3,
+    Voice = 4,
+    Email = 5
+  }
 }
