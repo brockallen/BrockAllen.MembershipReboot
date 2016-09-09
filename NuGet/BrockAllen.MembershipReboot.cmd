f36d65d89e60b5a8838e1873c06aca33278bb1e4
Brock Allen <brockallen@gmail.com>
2016-09-09 12:26:58 -0400

    add other languages

--- NuGet/BrockAllen.MembershipReboot.cmd
+++ NuGet/BrockAllen.MembershipReboot.cmd
@@ -10,6 +10,14 @@ xcopy ..\build\da\BrockAllen.MembershipReboot.resources.dll BrockAllen.Membershi
 mkdir BrockAllen.MembershipReboot\lib\net45\de
 xcopy ..\build\de\BrockAllen.MembershipReboot.resources.dll BrockAllen.MembershipReboot\lib\net45\de /y
 
+::FI
+mkdir BrockAllen.MembershipReboot\lib\net45\fi
+xcopy ..\build\fi\BrockAllen.MembershipReboot.resources.dll BrockAllen.MembershipReboot\lib\net45\fi /y
+
+::FR
+mkdir BrockAllen.MembershipReboot\lib\net45\fr
+xcopy ..\build\fr\BrockAllen.MembershipReboot.resources.dll BrockAllen.MembershipReboot\lib\net45\fr /y
+
 ::NL
 mkdir BrockAllen.MembershipReboot\lib\net45\nl-nl
 xcopy ..\build\nl-nl\BrockAllen.MembershipReboot.resources.dll BrockAllen.MembershipReboot\lib\net45\nl-nl /y
@@ -17,6 +25,10 @@ xcopy ..\build\nl-nl\BrockAllen.MembershipReboot.resources.dll BrockAllen.Member
 ::NO
 mkdir BrockAllen.MembershipReboot\lib\net45\no
 xcopy ..\build\no\BrockAllen.MembershipReboot.resources.dll BrockAllen.MembershipReboot\lib\net45\no /y
+
+::PT-BR
+mkdir BrockAllen.MembershipReboot\lib\net45\pt-br
+xcopy ..\build\pt-br\BrockAllen.MembershipReboot.resources.dll BrockAllen.MembershipReboot\lib\net45\pt-br /y
 
 ::RU
 mkdir BrockAllen.MembershipReboot\lib\net45\ru-ru
